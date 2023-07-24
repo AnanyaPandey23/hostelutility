3fe67f961065aaa5707a1c9b37be1ccf809432f5
Ananya <apandey2_be20@thapar.edu>
2023-07-24 19:09:38 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const bodyparser = require("body-parser");
-require("dotenv").config();
 const cors = require("cors");
 const db = require("./DbConnection/DbConfig");
 const userRoute = require("./Routes/UserRouter");
@@ -15,7 +14,4 @@ app.use(
 );
 app.use("/user", userRoute);
 
-app.listen(
-  process.env.PORT,
-  console.log(`server listening to the port ${process.env.PORT}`)
-);
+module.exports = app;
